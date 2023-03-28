60e28d61f51365c2a0cfe9f7069f90dc142eb905
boing-86 <boing0806@sookmyung.ac.kr>
2023-03-28 22:44:11 +0900

    [Feat] String, Comment Tokenize

--- hello.c
+++ hello.c
@@ -1,11 +1,13 @@
 int main(void){
 	float _b2;	// Variable
 	_b2 = 40.12359E+2f;
-	_b2 *= (.23F + (001024 % '\t'));
+	_b2 *= (.23F + (001024 % '\\'));
 	printf("Hello\tWorld!\n");
 	/**
 	 * End of source code
 	 ***/
 	return 0;
+
+	
 }
 

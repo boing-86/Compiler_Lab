48bb7bbd36ef0b4b365582443c3ff27cfdefe065
boing-86 <boing0806@sookmyung.ac.kr>
2023-03-28 21:02:31 +0900

    [feat] Char Const 정규식에서 활용이 정의된 기호 추가

--- hello.c
+++ hello.c
@@ -1,8 +1,7 @@
 int main(void){
 	float _b2;	// Variable
-	char a = '/';
 	_b2 = 40.12359E+2f;
-	_b2 *= (.23F + (001024 % '\v'));
+	_b2 *= (.23F + (001024 % '\t'));
 	printf("Hello\tWorld!\n");
 	/**
 	 * End of source code
